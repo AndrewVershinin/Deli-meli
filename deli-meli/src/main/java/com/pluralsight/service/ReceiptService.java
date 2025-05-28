966dd28e652c0cd4d740162a038594ca1f20704c
Anrew <andrvolg.job@gmail.com>
2025-05-28 10:37:46 -0400

    receiptService class to save order in file is reade

--- deli-meli/src/main/java/com/pluralsight/service/ReceiptService.java
+++ deli-meli/src/main/java/com/pluralsight/service/ReceiptService.java
@@ -1,5 +1,33 @@
 package com.pluralsight.service;
 
+import com.pluralsight.items.Order;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class ReceiptService {
-    // TODO: Save order receipt to file with timestamp
+    public static void saveOrderToFile(Order order) {
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss"));
+
+        String fileName = "receipts/" + timestamp + ".txt";
+
+        try {
+            // will create folder if it doesn't exist
+            File folder = new File("receipts");
+
+            if(!folder.exists()){
+                folder.mkdirs();
+            }
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
+            writer.write(order.toString());
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
